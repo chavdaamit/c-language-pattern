5420d1ef98d44163f29255ec74fc1c36488e0703
chavda amit <adchavda7777@gmail.com>
2025-11-25 16:31:24 +0530

    Add files via upload

--- ch-8_loops_with_space/q1.c
+++ ch-8_loops_with_space/q1.c
@@ -2,27 +2,28 @@
 
 int main(){
 
-
 int num=5;
 
 
-    for(int i=1;i<=num;i++){
+for (int i=1; i<=num;i++){
 
-    for(int j=1;j<=num-i;j++){
-            printf(" ");
-        }
-    for (int k=i;k>=1;k--){
-        printf("%d",k);
-    }
-    printf("\n");
+for (int j=1; j<=num-i;j++){
 
+    printf(" ");
+}
 
-    }
+for(int k=i; k>=1;k--){
 
+    printf("%d",k);
+}
 
 
+printf("\n");
+
+}
 
 
-return 0;
 
+
+    return 0;
 }
--- ch-8_loops_with_space/q2.c
+++ ch-8_loops_with_space/q2.c
@@ -1,28 +1,29 @@
-
 #include <stdio.h>
 
-
 int main(){
-
 
 int num=5;
 
 
-for(int i=1;i<=num;i++){
 
+for (int i=5; i>=1; i--){
 
-    for(int j=1;j<=num-i;j++){
+    for(int j=1;j<i;j++){
         printf(" ");
     }
 
-    for(int k=1;k<=i;k++){
 
-    printf("%d",k);
-    
+    for(int k=i; k<=num; k++){
+        printf("%d",k);
+    }
+
+    printf("\n");
+
 }
-printf("\n");
 
 
-}
+
+
+
     return 0;
 }
